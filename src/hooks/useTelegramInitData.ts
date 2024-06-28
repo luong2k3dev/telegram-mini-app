71b8efc3f708cb005ada28980212693afa1c08e8
luong2k3dev <tunhatluong2k3@gmail.com>
2024-06-28 16:20:57 +0700

    Fix

--- src/hooks/useTelegramInitData.ts
+++ src/hooks/useTelegramInitData.ts
@@ -1,12 +1,42 @@
 import { useEffect, useState } from "react";
 import { TelegramWebApps } from "telegram-webapps-types";
-import WebApp from '@twa-dev/sdk'
+import WebApp from "@twa-dev/sdk";
+import crypto from "crypto";
 /**
  * Hook to get the initial data from the Telegram Web Apps API already parsed.
  * @example
  * const { hash } = useTelegramInitData();
  * console.log({ hash });
  */
+const verifyTelegramWebAppData = (telegramInitData: string) => {
+  const botToken = "7404494799:AAGIyZaynaelpZd5_9gBOctvg3dCcEzBZVk";
+  // The data is a query string, which is composed of a series of field-value pairs.
+  const encoded = decodeURIComponent(telegramInitData);
+
+  // HMAC-SHA-256 signature of the bot's token with the constant string WebAppData used as a key.
+  const secret = crypto.createHmac("sha256", "WebAppData").update(botToken);
+
+  // Data-check-string is a chain of all received fields'.
+  const arr = encoded.split("&");
+  const hashIndex = arr.findIndex((str) => str.startsWith("hash="));
+  const hash = arr.splice(hashIndex)[0].split("=")[1];
+  // Sorted alphabetically
+  arr.sort((a, b) => a.localeCompare(b));
+  // In the format key=<value> with a line feed character ('\n', 0x0A) used as separator
+  // e.g., 'auth_date=<auth_date>\nquery_id=<query_id>\nuser=<user>
+  const dataCheckString = arr.join("\n");
+
+  // The hexadecimal representation of the HMAC-SHA-256 signature of the data-check-string with the secret key
+  const _hash = crypto
+    .createHmac("sha256", secret.digest())
+    .update(dataCheckString)
+    .digest("hex");
+
+  // If hash is equal, the data may be used on your server.
+  // Complex data types are represented as JSON-serialized objects.
+  return _hash === hash;
+};
+
 export function useTelegramInitData() {
   const [data, setData] = useState<TelegramWebApps.WebAppInitData>({});
 
@@ -24,6 +54,9 @@ export function useTelegramInitData() {
         initData[key] = firstLayerInitData[key];
       }
     }
+
+    const validData = verifyTelegramWebAppData(WebApp.initData);
+    initData['validData'] = validData.toString();
 
     setData(initData);
   }, []);
