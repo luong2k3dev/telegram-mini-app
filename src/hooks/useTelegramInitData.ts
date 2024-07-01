94a6f0a9798fbe4f971e7337f91efd247e558662
luong2k3dev <tunhatluong2k3@gmail.com>
2024-07-01 15:38:14 +0700

    Fix

--- src/hooks/useTelegramInitData.ts
+++ src/hooks/useTelegramInitData.ts
@@ -20,6 +20,14 @@ const verifyTelegramWebAppData = (telegramInitData: string) => {
   const arr = encoded.split("&");
   const hashIndex = arr.findIndex((str) => str.startsWith("hash="));
   const hash = arr.splice(hashIndex)[0].split("=")[1];
+
+  // Check expiration time
+  const authDateIndex = arr.findIndex((str) => str.startsWith("auth_date="));
+  const authDate = parseInt(arr.splice(authDateIndex, 1)[0].split("=")[1]);
+  const currentTime = Math.floor(Date.now() / 1000);
+  const timeDiff = currentTime - authDate;
+  const maxTimeDiff = 300;
+
   // Sorted alphabetically
   arr.sort((a, b) => a.localeCompare(b));
   // In the format key=<value> with a line feed character ('\n', 0x0A) used as separator
@@ -34,7 +42,7 @@ const verifyTelegramWebAppData = (telegramInitData: string) => {
 
   // If hash is equal, the data may be used on your server.
   // Complex data types are represented as JSON-serialized objects.
-  return _hash === hash;
+  return _hash === hash && timeDiff < maxTimeDiff;
 };
 
 export function useTelegramInitData() {
@@ -55,7 +63,7 @@ export function useTelegramInitData() {
       }
     }
 
-    const validData = verifyTelegramWebAppData("query_id=AAE5iqA5AgAAADmKoDlv2jnS&user=%7B%22id%22%3A5261789753%2C%22first_name%22%3A%22BD%22%2C%22last_name%22%3A%22On%20Ton%22%2C%22username%22%3A%22Naiha86%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1719820516&hash=dc113504422c5281c45ffb6cbd9a4395c20f082f1f97d7530f37b8667b38a694");
+    const validData = verifyTelegramWebAppData("query_id=AAE5iqA5AgAAADmKoDldrAZ8&user=%7B%22id%22%3A5261789753%2C%22first_name%22%3A%22BD%22%2C%22last_name%22%3A%22On%20Ton%22%2C%22username%22%3A%22Naiha86%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1719823065&hash=967268e8091665e30a395f01aec7c00ecfa50547b4be8d6b839348bbf295950c");
     initData['validData'] = validData.toString();
     initData['initData'] = WebApp.initData;
 
