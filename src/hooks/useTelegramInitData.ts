62a6d883b3fb150110ac780b27f20715a2ec6d2f
luong2k3dev <tunhatluong2k3@gmail.com>
2024-07-01 15:53:50 +0700

    Fix

--- src/hooks/useTelegramInitData.ts
+++ src/hooks/useTelegramInitData.ts
@@ -22,8 +22,8 @@ const verifyTelegramWebAppData = (telegramInitData: string) => {
   const hash = arr.splice(hashIndex)[0].split("=")[1];
 
   // Check expiration time
-  const authDateIndex = arr.findIndex((str) => str.startsWith("auth_date="));
-  const authDate = parseInt(arr.splice(authDateIndex, 1)[0].split("=")[1]);
+  const authDateStr = arr.find((str) => str.startsWith("auth_date="));
+  const authDate = authDateStr ? parseInt(authDateStr.split("=")[1]) : 0;
   const currentTime = Math.floor(Date.now() / 1000);
   const timeDiff = currentTime - authDate;
   const maxTimeDiff = 300;
@@ -63,9 +63,11 @@ export function useTelegramInitData() {
       }
     }
 
-    const validData = verifyTelegramWebAppData("query_id=AAE5iqA5AgAAADmKoDldrAZ8&user=%7B%22id%22%3A5261789753%2C%22first_name%22%3A%22BD%22%2C%22last_name%22%3A%22On%20Ton%22%2C%22username%22%3A%22Naiha86%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1719823065&hash=967268e8091665e30a395f01aec7c00ecfa50547b4be8d6b839348bbf295950c");
-    initData['validData'] = validData.toString();
-    initData['initData'] = WebApp.initData;
+    const validData = verifyTelegramWebAppData(
+      "query_id=AAE5iqA5AgAAADmKoDldrAZ8&user=%7B%22id%22%3A5261789753%2C%22first_name%22%3A%22BD%22%2C%22last_name%22%3A%22On%20Ton%22%2C%22username%22%3A%22Naiha86%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1719823065&hash=967268e8091665e30a395f01aec7c00ecfa50547b4be8d6b839348bbf295950c"
+    );
+    initData["validData"] = validData.toString();
+    initData["initData"] = WebApp.initData;
 
     setData(initData);
   }, []);
