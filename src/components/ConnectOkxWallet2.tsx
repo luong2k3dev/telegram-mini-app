b7055848c78bfa6cb72aa561d7f381220f82785e
luongtn-relipa <luongtn@relipasoft.com>
2024-10-14 16:48:59 +0700

    fix

--- src/components/ConnectOkxWallet2.tsx
+++ src/components/ConnectOkxWallet2.tsx
@@ -1,85 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { OKXTonConnectUI, THEME } from "@okxconnect/ui";
-import { Button } from "./styled/styled";
 
 const OkxConnectButton = () => {
-  const [address, setAddress] = useState("");
-  const [connected, setConnected] = useState(false);
-
-  const okxUI: any = new OKXTonConnectUI({
-    dappMetaData: {
-      name: "Red pocket",
-      icon: "https://web.telegram.org/2fa85bf1-9c48-470e-95de-d8e848bd53dc",
-    },
-    actionsConfiguration: {
-      returnStrategy: "tg://resolve",
-    },
-    uiPreferences: {
-      theme: THEME.DARK,
-    },
-    language: "en_US",
-    restoreConnection: true,
-  });
+  const [okxUI, setOkxUI] = useState<any>(null);
 
   useEffect(() => {
-    const restoreConnection = async () => {
-      try {
-        await okxUI.openModal();
-        const wallet = okxUI.wallet;
-        const account = okxUI.account;
-
-        if (wallet && account) {
-          setAddress(account.address);
-          setConnected(true);
-        }
-      } catch (error) {
-        console.log("Không có kết nối nào cần khôi phục");
-      }
-    };
-
-    restoreConnection();
+    const okxUIState: any = new OKXTonConnectUI({
+      dappMetaData: {
+        name: "Red pocket",
+        icon: "https://web.telegram.org/2fa85bf1-9c48-470e-95de-d8e848bd53dc",
+      },
+      buttonRootId: "okx-connect-button",
+      actionsConfiguration: {
+        returnStrategy: "tg://resolve",
+      },
+      uiPreferences: {
+        theme: THEME.DARK,
+      },
+      language: "en_US",
+      restoreConnection: true,
+    });
+    setOkxUI(okxUIState);
+    console.log("wallet", okxUIState.wallet);
   }, []);
 
-  const handleConnect = async () => {
-    try {
-      if (okxUI) {
-        // Mở modal kết nối
-        await okxUI.openModal();
-        const wallet = okxUI.wallet;
-        const account = okxUI.account;
-
-        if (wallet && account) {
-          setAddress(account.address);
-          setConnected(true);
-        }
-      }
-    } catch (error) {
-      console.log("Không thể kết nối ví:", error);
-    }
-  };
-
-  const handleDisconnect = async () => {
-    try {
-      await okxUI.disconnect();
-      setConnected(false);
-      setAddress("");
-    } catch (error) {
-      console.log("Không thể ngắt kết nối:", error);
-    }
-  };
-
-  return (
-    <div>
-      {connected ? (
-        <div>
-          <p>Đã kết nối với địa chỉ: {address}</p>
-          <Button onClick={handleDisconnect}>Ngắt kết nối</Button>
-        </div>
-      ) : (
-        <Button onClick={handleConnect}>Kết nối với OKX Wallet</Button>
-      )}
-    </div>
-  );
+  return <div id="okx-connect-button"></div>;
 };
 
 export default OkxConnectButton;
